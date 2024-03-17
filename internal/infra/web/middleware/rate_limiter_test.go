c1016a1d43505bf4516c8854d46391ff3efbc442
Caio Felipe de Araujo <c.felipe.araujo@gmail.com>
2024-03-17 12:34:59 -0300

    add token test

--- internal/infra/web/middleware/rate_limiter_test.go
+++ internal/infra/web/middleware/rate_limiter_test.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,7 +14,7 @@ func TestRateLimiterBlockByMaxReqAllowed(t *testing.T) {
 	svr := httptest.NewServer(RateLimiter(SomeHandler))
 	defer svr.Close()
 
-	// max duas requisicoes
+	// max duas requisicoes em 1 seg
 	err := setConfig("2", "IP", "1")
 	if err != nil {
 		t.Errorf("Error: %v", err)
@@ -27,7 +26,6 @@ func TestRateLimiterBlockByMaxReqAllowed(t *testing.T) {
 		t.Errorf("Error: %v", err)
 	}
 	defer res.Body.Close()
-	fmt.Printf("Status response 1: %v", res.StatusCode)
 	assert.Equal(t, res.StatusCode, http.StatusOK)
 
 	// segunda requisicao deve passar
@@ -36,7 +34,6 @@ func TestRateLimiterBlockByMaxReqAllowed(t *testing.T) {
 		t.Errorf("Error: %v", err)
 	}
 	defer res2.Body.Close()
-	fmt.Printf("Status response 2: %v", res2.StatusCode)
 	assert.Equal(t, res2.StatusCode, http.StatusOK)
 
 	// terceira requisicao deve bloquear..
@@ -45,7 +42,6 @@ func TestRateLimiterBlockByMaxReqAllowed(t *testing.T) {
 		t.Errorf("Error: %v", err)
 	}
 	assert.Equal(t, res3.StatusCode, http.StatusTooManyRequests)
-	fmt.Printf("Status response 3: %v", res3.StatusCode)
 	defer res3.Body.Close()
 
 	time.Sleep(1 * time.Second)
@@ -56,7 +52,6 @@ func TestRateLimiterBlockByMaxReqAllowed(t *testing.T) {
 		t.Errorf("Error: %v", err)
 	}
 	assert.Equal(t, res4.StatusCode, http.StatusOK)
-	fmt.Printf("Status response 4: %v", res4.StatusCode)
 	defer res4.Body.Close()
 }
 
@@ -64,46 +59,78 @@ func TestRateLimiterBlockByIP(t *testing.T) {
 	svr := httptest.NewServer(RateLimiter(SomeHandler))
 	defer svr.Close()
 
-	// max duas requisicoes por IP
+	// max duas requisicoes por IP em 1 seg
 	err := setConfig("2", "IP", "1")
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
 
 	// primeira requisicao deve passar
-	res, err := http.Get(svr.URL)
-	if err != nil {
-		t.Errorf("Error: %v", err)
-	}
+	res, err := doRequest("X-Real-Ip", "192.168.222.2", svr)
 	defer res.Body.Close()
 	assert.Equal(t, res.StatusCode, http.StatusOK)
 
 	// segunda requisicao deve passar
-	res2, err := http.Get(svr.URL)
-	if err != nil {
-		t.Errorf("Error: %v", err)
-	}
+	res2, err := doRequest("X-Real-Ip", "192.168.222.2", svr)
 	defer res2.Body.Close()
 	assert.Equal(t, res2.StatusCode, http.StatusOK)
 
 	// terceira requisicao deve bloquear..
-	res3, err := http.Get(svr.URL)
+	res3, err := doRequest("X-Real-Ip", "192.168.222.2", svr)
+	assert.Equal(t, res3.StatusCode, http.StatusTooManyRequests)
+	defer res3.Body.Close()
+
+	// uma quarta requisicao, com outro IP, deve passar
+	res4, err := doRequest("X-Real-Ip", "0.0.0.0", svr)
+	assert.Equal(t, res4.StatusCode, http.StatusOK)
+	defer res4.Body.Close()
+}
+
+func TestRateLimiterBlockByToken(t *testing.T) {
+	svr := httptest.NewServer(RateLimiter(SomeHandler))
+	defer svr.Close()
+
+	// max duas requisicoes por Token em 1 seg
+	err := setConfig("2", "TOKEN", "1")
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	// primeira requisicao deve passar
+	res1, err := doRequest("API_KEY", "abc123", svr)
+	assert.Equal(t, res1.StatusCode, http.StatusOK)
+	defer res1.Body.Close()
+
+	// segunda requisicao deve passar
+	res2, err := doRequest("API_KEY", "abc123", svr)
+	assert.Equal(t, res2.StatusCode, http.StatusOK)
+	defer res2.Body.Close()
+
+	// terceira requisicao deve bloquear..
+	res3, err := doRequest("API_KEY", "abc123", svr)
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
 	assert.Equal(t, res3.StatusCode, http.StatusTooManyRequests)
 	defer res3.Body.Close()
 
-	// uma quarta requisicao, com outro IP, deve passar
-	req4, err := http.NewRequest("GET", svr.URL, nil)
-	req4.Header.Set("X-Real-Ip", "0.0.0.0")
+	// uma quarta requisicao, com outro token, deve passar
+	res4, err := doRequest("API_KEY", "abc321", svr)
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
-	httpclient := &http.Client{}
-	res4, err := httpclient.Do(req4)
 	assert.Equal(t, res4.StatusCode, http.StatusOK)
 	defer res4.Body.Close()
+}
+
+func doRequest(key, value string, svr *httptest.Server) (*http.Response, error) {
+	httpclient := &http.Client{}
+	req, err := http.NewRequest("GET", svr.URL, nil)
+	req.Header.Set(key, value)
+	if err != nil {
+		return nil, err
+	}
+	return httpclient.Do(req)
 }
 
 func setConfig(maxReqAllowed, blockMethod, blockTime string) error {
